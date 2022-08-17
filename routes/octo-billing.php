c4884df2e06c235d284fbabcbc9a4778e98ddb1b
a21ns1g4ts <atila.danvi@outlook.com>
2022-08-17 09:19:03 -0300

    Change webhook route

--- routes/octo-billing.php
+++ routes/octo-billing.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use OctoBilling\Http\Controllers\BillingWebhook;
 
-Route::post('billing/stripe/webhook', [BillingWebhook::class, 'handleWebhook'])->name('octo-billing.webhook');
+Route::post('octo-billing/stripe/webhook', [BillingWebhook::class, 'handleWebhook'])->name('octo-billing.webhook');
