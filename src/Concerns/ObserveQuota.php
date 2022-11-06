2419f1195db01f65d233a8aa1c626a069a6d574f
a21ns1g4ts <atila.danvi@outlook.com>
2022-11-06 20:24:15 -0300

    Add quoto observer

---
+++ src/Concerns/ObserveQuota.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Octo;
+
+use Illuminate\Support\Facades\Auth;
+
+trait ObserveQuota
+{
+    protected static function booted()
+    {
+        parent::booted();
+
+        static::creating(function ($model) {
+            $user = Auth::user();
+
+            $subscription = $user?->currentSubscription;
+
+            if (!$subscription) {
+                return;
+            }
+
+            $features = $subscription->features;
+
+            /** @var \Octo\Billing\Feature $featureObservable */
+            $featureObservable = $features->filter(fn ($f) => $f->getModel() === self::class)->first();
+
+            if ($featureObservable) {
+                if ($subscription->getRemainingQuota($featureObservable) <= 0) {
+                    abort(403, 'Subscription quota exceeded');
+                }
+            }
+        });
+
+
+        static::created(function ($model) {
+            $user = Auth::user();
+
+            $subscription = $user?->currentSubscription;
+
+            if (!$subscription) {
+                return;
+            }
+
+            $features = $subscription->features;
+
+            /** @var \Octo\Billing\Feature $featureObservable */
+            $featureObservable = $features->filter(fn ($f) => $f->getModel() === self::class)->first();
+
+            if ($featureObservable) {
+                $subscription->recordFeatureUsage($featureObservable->getId(), $featureObservable->calculeUsage($model));
+            }
+        });
+
+        static::deleted(function ($model) {
+            $user = Auth::user();
+
+            $subscription = $user?->currentSubscription;
+
+            if (!$subscription) {
+                return;
+            }
+
+            $features = $subscription->features;
+
+            /** @var \Octo\Billing\Feature $featureObservable */
+            $featureObservable = $features->filter(fn ($f) => $f->getModel() === self::class)->first();
+
+            if ($featureObservable) {
+                $subscription->reduceFeatureUsage($featureObservable->getId(), $featureObservable->calculeUsage($model));
+            }
+        });
+    }
+}
